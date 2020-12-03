4c038f1f1b107005f4538ab4cc212f9c7af070da
phatdroid94 <phatdroid94@gmail.com>
2020-12-03 21:15:44 +0700

    demo

--- app/src/main/java/com/example/firebaseadmob25082020/MainActivity.java
+++ app/src/main/java/com/example/firebaseadmob25082020/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.firebaseadmob25082020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
@@ -19,10 +23,43 @@ public class MainActivity extends AppCompatActivity {
 //        id ung dung : ca-app-pub-1160054188680269~3484960032
 //        id quang cao : ca-app-pub-1160054188680269/2134373431
 
-        MobileAds.initialize(this, );
+        MobileAds.initialize(this);
 
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+        mAdView.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+            }
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError loadAdError) {
+                super.onAdFailedToLoad(loadAdError);
+                Log.d("BBB",loadAdError.getMessage());
+            }
+
+            @Override
+            public void onAdOpened() {
+                super.onAdOpened();
+            }
+
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+            }
+
+            @Override
+            public void onAdClicked() {
+                super.onAdClicked();
+            }
+
+            @Override
+            public void onAdImpression() {
+                super.onAdImpression();
+            }
+        });
     }
 }
