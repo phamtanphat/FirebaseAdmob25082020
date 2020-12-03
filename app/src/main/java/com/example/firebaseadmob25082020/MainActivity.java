279a3626f686604ae5083d3350a65ce4c34eef83
phatdroid94 <phatdroid94@gmail.com>
2020-12-03 20:46:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firebaseadmob25082020/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.firebaseadmob25082020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        id ung dung : ca-app-pub-1160054188680269~3484960032
+//        id quang cao : ca-app-pub-1160054188680269/2134373431
+
+        MobileAds.initialize(this, );
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
